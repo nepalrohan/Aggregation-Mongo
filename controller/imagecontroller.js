8f18aebd606c4633856d85a59e491f1712b4dc17
nepalrohan <rohannepal098@gmail.com>
2025-03-15 14:12:05 +0545

    add sorting and pagination

--- controller/imagecontroller.js
+++ controller/imagecontroller.js
@@ -37,51 +37,64 @@ export const uploadImage = async (req, res) => {
 
 export const fetchImagesController = async (req, res) => {
   try {
+    const page = parseInt(req.query.page) || 1;
 
-const images = await Image.find({});
-if(images){
-    res.status(200).json({
-        success:true, 
-        data:images
-    })
-}
+    const limit = parseInt(req.query.limit) || 5;
+    const skip = (page - 1) * limit;
 
+
+    const sortBy = req.query.sortBy || 'createdAt';
+    const sortOrder = req.query.sortOrder === 'asc' ? 1: -1;
+
+    const totalimages = await Image.countDocuments();
+
+    const totalPages = Math.ceil(totalimages /limit);
+    const sortObj = {};
+
+    sortObj[sortBy]=sortOrder;
+    const images = await Image.find().sort(sortObj).skip(skip).limit(limit);
+
+    if (images) {
+      res.status(200).json({
+        success: true,
+        data: images,
+        currentPage:page,
+        totalPages:totalPages,
+        totalimages:totalimages,
+        
+      });
+    }
   } catch (error) {
     console.log(error);
   }
 };
 
-
-
-export const deleteImageController = async (req, res)=>{
+export const deleteImageController = async (req, res) => {
   try {
-    
-const getcurrentImageId = req.params.id;
-const userId = req.userInfo.userId;
+    const getcurrentImageId = req.params.id;
+    const userId = req.userInfo.userId;
 
-const image = await Image.findById(getcurrentImageId);
-if(!image){
-  return res.json({
-    message:'Image not found'
-  })
-}
+    const image = await Image.findById(getcurrentImageId);
+    if (!image) {
+      return res.json({
+        message: "Image not found",
+      });
+    }
 
+    if (image.uploadedBy.toString() !== userId) {
+      return res.json({ message: "Not authorized" });
+    }
 
-if(image.uploadedBy.toString() !== userId){
-  return res.json({message:'Not authorized'})
-}
+    //delete image in cloudinary
+    await cloudinary.uploader.destroy(image.publicId);
 
-//delete image in cloudinary
-await cloudinary.uploader.destroy(image.publicId);
+    //delet image in database mongodb
+    await image.findByIdAndDelete(getcurrentImageId);
 
-//delet image in database mongodb
-await image.findByIdAndDelete(getcurrentImageId);
-
-res.json({
-  message:'Image deleted successfully!'
-})
-
+    res.json({
+      message: "Image deleted successfully!",
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
